508cfd453ebadd7d68afbe9b45f457565e14e630
Christopher Morrison <Chris.Morrison500@gmail.com>
2017-12-04 19:52:16 -0500

    See previous commit

--- Setup/SQL/functions/cascade_user_addition.sql
+++ Setup/SQL/functions/cascade_user_addition.sql
@@ -6,8 +6,11 @@ CREATE OR REPLACE FUNCTION "CALICO".cascade_user_addition()
   RETURNS trigger AS
 $BODY$
 BEGIN
+	INSERT INTO "CALICO"."User_Contact" 
+		VALUES('','','',NEW."User Key");
 	INSERT INTO "CALICO"."User_Education" 
 		VALUES('','',false,true,NEW."User Key");
+	
 	RETURN NEW;
 END;
 $BODY$
